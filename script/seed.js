4f35ced46cff85741a29634f9ebc7da355fd1ca9
Katie Agresta <katieagresta1@gmail.com>
2019-04-17 12:49:00 -0400

    passwords added #4

--- script/seed.js
+++ script/seed.js
@@ -3,58 +3,96 @@
 const db = require('../server/db')
 const {User, Movie} = require('../server/db/models')
 const users = [
- {"name": "Kylo", "email": "kylo@ren.com", "imageUrl": "kylo.png"},
- {"name": "Rey", "email": "rey@milleniumfalcon.com", "imageUrl": "rey.png"},
- {"name": "Finn", "email": "finn@resistance.com", "imageUrl": "finn.png"},
- {"name": "Poe", "email": "poe@resistance.com", "imageUrl": "poe.png"}
+  {
+    name: 'Kylo',
+    email: 'kylo@ren.com',
+    imageUrl: 'kylo.png',
+    password: 'password'
+  },
+  {
+    name: 'Rey',
+    email: 'rey@milleniumfalcon.com',
+    imageUrl: 'rey.png',
+    password: 'password'
+  },
+  {
+    name: 'Finn',
+    email: 'finn@resistance.com',
+    imageUrl: 'finn.png',
+    password: 'password'
+  },
+  {
+    name: 'Poe',
+    email: 'poe@resistance.com',
+    imageUrl: 'poe.png',
+    password: 'password'
+  }
 ]
 
 const movies = [
- {
-   "id":1,
-   "title":"Lion King",
-   "genre":"Animated",
-   "imageUrl":"http://www.gstatic.com/tv/thumb/movieposters/14113286/p14113286_p_v8_ac.jpg",
-   "price":2.99
- },
- {
-   "id":2,
-   "title":"Lord of the rings",
-   "genre":"Adventure,.Drama,Fantacy",
-   "imageUrl":"https://upload.wikimedia.org/wikipedia/en/c/c3/The_Lord_of_the_Rings_trilogy_poster.jpg",
-   "price":3.99,
- },
- {
-   "id":3,
-   "title":"it","genre":"Horror","imageUrl":"http://www.gstatic.com/tv/thumb/v22vodart/13610713/p13610713_v_v8_ac.jpg","price":4.99
- },
- {
-   "id":4,"title":"Brides Maid","genre":"Comedy","imageUrl":"http://www.gstatic.com/tv/thumb/v22vodart/8414185/p8414185_v_v8_ac.jpg","price":1.99
- },
- {
-   "id":5,"title":"Harry Potter","genre":"Adventure,.Drama,Fantacy","imageUrl":"https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg","price":3.99
- },
+  {
+    id: 1,
+    title: 'Lion King',
+    genre: 'Animated',
+    imageUrl:
+      'http://www.gstatic.com/tv/thumb/movieposters/14113286/p14113286_p_v8_ac.jpg',
+    price: 2.99
+  },
+  {
+    id: 2,
+    title: 'Lord of the rings',
+    genre: 'Adventure,.Drama,Fantacy',
+    imageUrl:
+      'https://upload.wikimedia.org/wikipedia/en/c/c3/The_Lord_of_the_Rings_trilogy_poster.jpg',
+    price: 3.99
+  },
+  {
+    id: 3,
+    title: 'it',
+    genre: 'Horror',
+    imageUrl:
+      'http://www.gstatic.com/tv/thumb/v22vodart/13610713/p13610713_v_v8_ac.jpg',
+    price: 4.99
+  },
+  {
+    id: 4,
+    title: 'Brides Maid',
+    genre: 'Comedy',
+    imageUrl:
+      'http://www.gstatic.com/tv/thumb/v22vodart/8414185/p8414185_v_v8_ac.jpg',
+    price: 1.99
+  },
+  {
+    id: 5,
+    title: 'Harry Potter',
+    genre: 'Adventure,.Drama,Fantacy',
+    imageUrl: 'https://timedotcom.files.wordpress.com/2014/07/301386_full1.jpg',
+    price: 3.99
+  }
 ]
 
-
-
 async function seed() {
-    try{
-  await db.sync({force: true})
-  console.log('db synced!')
+  try {
+    await db.sync()
+    console.log('db synced!')
 
-    await Promise.all(users.map(user => {
-      return User.create(user)
-    }))
-    await Promise.all(movies.map(movie => {
-      return Movie.create(movie)
-    }))
-  }catch(err){console.error(err)}
+    await Promise.all(
+      users.map(user => {
+        return User.create(user)
+      })
+    )
+    await Promise.all(
+      movies.map(movie => {
+        return Movie.create(movie)
+      })
+    )
+  } catch (err) {
+    console.error(err)
+  }
 
   console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
-
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
@@ -79,8 +117,6 @@ async function runSeed() {
 if (module === require.main) {
   runSeed()
 }
-
-
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
 module.exports = seed
