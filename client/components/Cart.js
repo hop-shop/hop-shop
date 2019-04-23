e55e1428a5ececf2ae53d490146b9cc6754375e2
Alex Grazier <kylealexandra@gmail.com>
2019-04-23 14:54:49 -0400

    starts edits to cart component

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -2,37 +2,48 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {getCartThunk} from '../store/cart'
 
-
 export class DisconnectedCart extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchCart(this.props.match.params.id)
   }
-  render(){
+  render() {
     const cart = this.props.cart
-    return (<div>
-      {cart && cart.length ?
+    return (
       <div>
-    {cart.map(cart=>{
-        return (
-        <div key={cart.userId}>
-          <h3>{cart.movie.title}</h3>
-          <img src={cart.movie.imageUrl} />
-          <span>{cart.movie.price}</span>
-
-        </div>
-        )})}
-        <div>
-          <span>Total Price: {cart.reduce((a,b)=>{return +(a+b.movie.price)},0)}</span>
+        {cart && cart.length ? (
+          <div>
+            {cart.map(cartItem => {
+              return (
+                <div key={cartItem.movieId}>
+                  <h3>{cartItem.movie.title}</h3>
+                  <img
+                    src={cartItem.movie.imageUrl}
+                    className="img-thumbnail"
+                  />
+                  <span>{cartItem.movie.price}</span>
+                </div>
+              )
+            })}
+            <div>
+              <span>
+                Total Price:{' '}
+                {cart.reduce((a, b) => {
+                  return +(a + b.movie.price)
+                }, 0)}
+              </span>
+            </div>
           </div>
-          </div>
-        :'Loading'}
-        </div>)
+        ) : (
+          'Loading'
+        )}
+      </div>
+    )
   }
 }
 
 const mapStateToProps = state => {
   return {
-    cart:state.cart
+    cart: state.cart
   }
 }
 
@@ -44,7 +55,6 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export const Cart = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(DisconnectedCart)
+export const Cart = connect(mapStateToProps, mapDispatchToProps)(
+  DisconnectedCart
+)
