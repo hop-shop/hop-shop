20f2cf7ed90e7c68f12345396d382d5ba666b24e
Katie Agresta <katieagresta1@gmail.com>
2019-04-24 01:33:28 -0400

    final changes for the night

--- client/components/carousel.js
+++ client/components/carousel.js
@@ -7,14 +7,12 @@ class carousel extends Component {
       <div className="jumbotron">
         <h1 className="display-4">Hopflix has the best flix!</h1>
         <p className="lead">
-          This is a simple hero unit, a simple jumbotron-style component for
-          calling extra attention to featured content or information.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
         </p>
-        <hr className="my-4" />
-        <p>
-          It uses utility classes for typography and spacing to space content
-          out within the larger container.
-        </p>
+
         <Link to="/movies">
           <button className="btn btn-primary btn-lg" href="#" role="button">
             Browse Movies
